4c699e65cce65da82fca81f6a379f2dc125c9597
Dingani Brian Nkala <dinganibrian@gmail.com>
2023-10-16 13:56:21 +0000

    Modified s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/dbnkala-hcc-isv/s3-bucket/aws"
+  version = "3.15.1"
   bucket_prefix = ${var.prefix}
-
-  versioning = {
-    enabled = true
-  }
-
 }
